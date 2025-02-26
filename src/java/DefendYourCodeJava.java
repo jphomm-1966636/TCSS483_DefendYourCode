37802715a89984b1131dbfe234be0a22991d42d1
jphomm-196636 <jphomm@uw.edu>
2025-02-25 16:20:15 -0800

    create starting structure of assignment

---
+++ src/java/DefendYourCodeJava.java
@@ -0,0 +1,11 @@
+package java;
+
+public class DefendYourCodeJava {
+    public static void main(String[] args) {
+        startingMenu();
+    }
+
+    private static void startingMenu() {
+        System.out.println("Please choose one of the following options");
+    }
+}
